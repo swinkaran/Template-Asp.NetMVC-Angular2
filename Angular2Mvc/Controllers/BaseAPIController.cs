46adcaca9b0b4731e92496a82fa6061757d318ac
Swinkaran <swinkaran@hotmail.com>
2017-08-23 15:57:45 +1000

    Project initialization ans setup
    Project initialization ans setup
    - MVC project included
    - Angular configuration completed
    - Basic controller created

---
+++ Angular2Mvc/Controllers/BaseAPIController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Angular2Mvc.DBContext;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Angular2Mvc.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+
+        protected readonly EmployeeTstEntities EmployeeDB = new EmployeeTstEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ Angular2Mvc/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Angular2Mvc.DBContext;
+using Newtonsoft.Json;
+using System.Text;
+using System.Data.Entity;
+
+namespace Angular2Mvc.Controllers
+{
+    public class EmployeeAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(EmployeeDB.TblEmployees.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]TblEmployee value)
+        {
+            EmployeeDB.TblEmployees.Add(value);
+            return ToJson(EmployeeDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]TblEmployee value)
+        {
+            EmployeeDB.Entry(value).State = EntityState.Modified;
+            return ToJson(EmployeeDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            EmployeeDB.TblEmployees.Remove(EmployeeDB.TblEmployees.FirstOrDefault(x => x.Id == id));
+            return ToJson(EmployeeDB.SaveChanges());
+        }
+    }
+}
