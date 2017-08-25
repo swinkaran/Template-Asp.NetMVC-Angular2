b0e5b81e8f2ae888225d17c84d0ef7a09ed7042d
Swinkaran <swinkaran@hotmail.com>
2017-08-25 17:06:02 +1000

    App module added
    App module added

---
+++ Angular2Mvc/app/app.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule],
+    declarations: [],
+    providers: [],
+    bootstrap:[]
+})
+
